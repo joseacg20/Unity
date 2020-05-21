d38c8da5f2a7b75fa04ca6e159967d3d1bb57449
Jose Angel <joseacg20@gmail.com>
2020-05-21 18:41:58 -0500

    Primer commit

---
+++ Assets/Scripts/DNA.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    public int State;
+    public Color Initial_Color;
+    SpriteRenderer Square_SR;
+
+    // Start is called before the first frame update
+
+    void OnMouseDown() {
+        if (State == 1) {
+            State = 0;
+        } else if(State == 0) {
+            State = 1;
+        }
+    }
+    void Start() {
+        Square_SR = GetComponent<SpriteRenderer>();
+        Square_SR.color = Initial_Color;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(State == 1) {
+            Square_SR.color = Color.white;
+        } else if(State == 0) {
+            Square_SR.color = Color.black;
+        }
+    }
+}
---
+++ Assets/Scripts/GameOfLife.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOfLife : MonoBehaviour
+{
+    public Vector2Int Board_Size;
+    public GameObject[, ] Board;
+    public GameObject Prefab_Square;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Board = new GameObject[Board_Size.x, Board_Size.y];
+        for (int i = 0; i < Board_Size.x; i++) {
+            for (int j = 0; j < Board_Size.y; j++) {
+                Vector3 Position = new Vector3(i = Board_Size.x/2, j = Board_Size.y/2, 0);
+                Board[i,j] = Instantiate(Prefab_Square, Position, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
